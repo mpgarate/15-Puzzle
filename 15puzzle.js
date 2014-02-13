123ab4d5b2072a99ad5ad4a4a17b0ce67f3e3981
Michael Garate <mpgarate@gmail.com>
2014-02-13 18:56:25 -0500

    it works for 3x3

--- 15puzzle.js
+++ 15puzzle.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
-    // 15 letters 
-    var goal_state = ['a', 'b', 'c', 'd',   // 0  1  2  3
-                       'e', 'f', 'g', 'h',   // 4  5  6  7
-                       'i', 'j', 'k', 'l',   // 8  9  10 11
-                       'm', 'n', 'o', ' '];  // 12 13 14 15
+    // 9 letters 
+    var goal_state = ['a', 'b', 'c',  // 0 1 2
+                      'd', 'e', 'f',  // 3 4 5
+                      'g', 'h', ' ']; // 6 7 8
 
-    var jumbled_state = ['a', 'b', 'c', 'd', // 0  1  2  3
-                       'e', ' ', 'f', 'g',   // 4  5  6  7
-                       'i', 'j', 'k', 'h',   // 8  9  10 11
-                       'm', 'n', 'o', 'l'];  // 12 13 14 15
+    var jumbled_state = ['a', 'b', ' ',  // 0 1 2
+                         'd', 'e', 'c',  // 3 4 5
+                         'g', 'h', 'f']; // 6 7 8
 
     // Board Collection based on JS Array
     function BoardCollection() { };
@@ -16,16 +14,16 @@ $(document).ready(function () {
 
     BoardCollection.prototype.contains = function(object){
         var found = true;
-        for (var i = 0; i < this.size; i++){
-          for (var j = 0; j < this[i].size; j++){
-            if (this[i][j] !== object[j]){
+        for (var i = 0; i < this.length; i++){
+          for (var j = 0; j < this[i].state.length; j++){
+            if (this[i].state[j] !== object.state[j]){
               found = false;
               break;
             }
-          }
+          } 
           if (found !== false){
             return true;
-          }
+          } 
         }
         return false;
       }
@@ -44,20 +42,16 @@ $(document).ready(function () {
     }
 
     Board.prototype.isInSameRow = function(i1,i2){
-      if (i1 < 4 && i2 < 4){
+      if (i1 < 3 && i2 < 3){
         //row 1
         return true;
       }
-      else if (i1 < 8 && i2 < 8){
+      else if (i1 < 6 && i2 < 6){
         //row 2
         return true;
       }
-      else if (i1 < 12 && i2 < 12){
+      else if (i1 < 9 && i2 < 9){
         //row 3
-        return true;
-      }
-      else if (i1 < 14 && i2 < 14){
-        //row 4
         return true;
       }
       else {
@@ -67,8 +61,8 @@ $(document).ready(function () {
 
     Board.prototype.touches = function(i1,i2){
       // if is in same column
-      if (i1 % 4 === i2 % 4){
-        if (Math.abs(i1 - i2) === 4){
+      if (i1 % 3 === i2 % 3){
+        if (Math.abs(i1 - i2) === 3){
           return true;
         }
       }
@@ -98,18 +92,13 @@ $(document).ready(function () {
     }
 
     Board.prototype.swap = function(i1,i2,v){
-      if (this.isValidSwap(i1,i2)){
-        var old_i1 = this.state[i1];
-        var old_i2 = this.state[i2];
-        this.state[i1] = old_i2;
-        this.state[i2] = old_i1;
-        if (this.visible === true){
-          $(".steps").append("<li>swapped \"" + old_i1 + "\" with \"" + old_i2 + "\"</li>");
-          this.draw();
-        }
-      }
-      else{
-        console.log("FATAL: tried to perform illegal swap.");
+      var old_i1 = this.state[i1];
+      var old_i2 = this.state[i2];
+      this.state[i1] = old_i2;
+      this.state[i2] = old_i1;
+      if (this.visible === true){
+        $(".steps").append("<li>swapped \"" + old_i1 + "\" with \"" + old_i2 + "\"</li>");
+        this.draw();
       }
     }
 
@@ -118,28 +107,35 @@ $(document).ready(function () {
     }
 
     Board.prototype.isGoalState = function(){
-      return (this.state === goal_state);
+      for (var i = 0; i < this.state.length; i++){
+        if(this.state[i] !== goal_state[i]){
+          return false;
+        }
+      }
+      return true;
     }
 
     // Use iterative deepening to solve
     Board.prototype.solve = function(){
-      this.visible = true;
 
-      var queue = []; // FIFO Queue
+      var queue = new BoardCollection(); // FIFO Queue
       queue.push(this);
       var finished = new BoardCollection();
 
-      while(queue.length > 0){
+      while(queue.length < 100){
         var new_board = queue.pop();
+        if (new_board.isGoalState()){
+          return new_board;
+        }
         finished.push(new_board);
-        for (var i = 0; i < 17; i++){
+        for (var i = 0; i < 9; i++){
           if(new_board.isValidSwap(i, new_board.blank_space())){
             var child = new Board(new_board.state.slice(0));
             child.swap(i, child.blank_space());
+            console.log(child.state);
             if(!finished.contains(child)){
-              if(queue.indexOf(child) === -1){
+              if(!queue.contains(child)){
                 queue.push(child);
-                console.log(queue.length);
                 if (child.isGoalState()){
                   console.log("found board");
                   return child; //or maybe our steps instead
@@ -148,13 +144,12 @@ $(document).ready(function () {
             }
           }
         }
+        console.log(queue.length);
       }
-
-      this.visible = true;
     }
 
 
-    board = new Board(jumbled_state);
+    var board = new Board(jumbled_state);
     board.draw();
 
     // initialize the board
